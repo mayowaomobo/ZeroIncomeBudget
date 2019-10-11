80c13f67a095182881e6d0556104ad95a8d0d34c
Mayowa Omobo <mayowaomobo@Mayowas-MacBook-Pro.local>
2019-10-10 22:31:30 -0400

    lmaooooooooo:|ooooooo it be like that sometimes

--- src/data/percentages.js
+++ src/data/percentages.js
@@ -3,26 +3,53 @@ const readlineSync = require('readline-sync')
 
 const percentages = (info) => { 
 MAX = 100, MIN = 0, value = (MAX/2), key = "", test = 100;
-for (var goals of info.info[1].factors){
-    
+
+
   
-  console.log('\n' + (new Array(30)).join(' ') +
-  'Percentage allocated to ' + goals+ '\n');
-console.log('' + (new Array(30)).join(' ') +
-  '[Z] <- -> [X]  FIX: [SPACE]\n');
-while (true) {
-  console.log('\x1B[1A\x1B[K|' +
-    (new Array(value + 1)).join('-') + 'O' +
-    (new Array(MAX - value + 1)).join('-') + '| ' + value);
-  key = readlineSync.keyIn('',
-    {hideEchoBack: true, mask: '', limit: 'zx '});
-  if (key === 'z') { if (value > MIN) { value--; } }
-  else if (key === 'x') { if (value < MAX) { value++; } }
-  else { break; }
-}
-console.log('\nA value the user requested: ' + value);
-console.log(goals)
-}
+    // do{
+      for (var goals of info.info[1].factors){
+      console.log(test)
+      console.log(goals)
+      input = readlineSync.question("Percentage allocated to " + goals)
+      
+      if (input < test){
+        test -= input;
+        
+      }
+      else {
+        console.log("More than 100% allocated")
+        return
+      }
+      }
+    // }while(input < test)
+  
+  //         console.log('\n' + (new Array(30)).join(' ') +
+  //         'Percentage allocated to ' + goals+ '\n');
+  //       console.log('' + (new Array(30)).join(' ') +
+  //         '[Z] <- -> [X]  FIX: [SPACE]\n');
+
+
+  //   while (true) {
+
+  //   console.log('\x1B[1A\x1B[K|' +
+  //   (new Array(value + 1)).join('-') + 'O' +
+  //   (new Array(MAX - value + 1)).join('-') + '| ' + value);
+
+  //   key = readlineSync.keyIn('',
+  //   {hideEchoBack: true, mask: '', limit: 'zx '});
+
+  // if (key === 'z') { 
+  //   if (value > MIN) {
+  //      value--; } }
+
+  // else if (key === 'x') { if (value < MAX) { value++; } }
+
+  // else { break; }
+  // value = test - value;
+// }
+// console.log('\nA value the user requested: ' + value);
+// console.log(goals)
+
 
 }
 
