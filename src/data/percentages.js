9395207e42458e78a619cfc79ab3a11e4f222502
Mayowa <mayowaomobo@gmail.com>
2019-10-28 19:39:59 -0400

    cleaned up percentages.js

--- src/data/percentages.js
+++ src/data/percentages.js
@@ -3,30 +3,24 @@ const readlineSync = require('readline-sync')
 input = false
 const percentages = (info) => { 
 percentage = 100
-number1 = 0
+starter = 0
 // console.log("this is percentages")
   
 // do{
   for (let factors in info.userInfo[1].factors){
-    number1++
-    factorPercent = readlineSync.questionInt((number1)+"/"+info.userInfo[1].factors.length + " Percent allocated to " + info.userInfo[1].factors[factors] + "; (" + percentage + "% available) ? ")
+    starter++
+    factorPercent = readlineSync.questionInt((starter)+"/"+info.userInfo[1].factors.length + " Percent allocated to " + info.userInfo[1].factors[factors] + "; (" + percentage + "% available) ? ")
     if (factorPercent > percentage || factorPercent < 0){
     while (factorPercent > percentage || factorPercent < 0){
       console.log("invalid input");
       factorPercent = readlineSync.questionInt("Percent allocated to " + info.userInfo[1].factors[factors] + "? " + percentage + "% available")
-      input = true
     }
-   } 
-      percentage = percentage - factorPercent
-    
+  } 
+      percentage = percentage - factorPercent  
+      info.userInfo[0].percentages.push({
+        percentage
+    })  
   }
-//   console.log("Hello")
-    
-//   }while(x == x)
-// }
-// for (let factors in info.userInfo[1].factors){
-  // console.log("The first factor is " + info.userInfo[1].factors[factors])
-      // }
-      console.log("done")
-    }
+}
+
 module.exports = percentages
