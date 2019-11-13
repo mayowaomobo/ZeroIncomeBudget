1eb416e12ca318c3c598799e205175f65336af9d
Mayowa <mayowaomobo@gmail.com>
2019-11-12 20:54:02 -0500

    removed random folder

--- random/callback.js
+++
@@ -1,8 +0,0 @@
-function doHomework(subject, callback) {
-    console.log(`Starting my ${subject} homework.`);
-    callback();
-  }
-  function alertFinished(){
-    console.log('Finished my homework');
-  }
-  doHomework('math', alertFinished);
